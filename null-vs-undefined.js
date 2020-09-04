ff939d9c49bfd5790395fe7deaa9459a5167929b
Bayzid Alim <67591935+bayzidalim@users.noreply.github.com>
2020-09-04 17:53:22 +0600

    commited

---
+++ null-vs-undefined.js
@@ -0,0 +1,11 @@
+function add(num1, num2){
+    console.log(num1+num2);
+}
+var result = add(12,13);
+console.log(result);
+return (result);
+const infff = {name: "Bayzid Alim", phone: 01335 };
+console.log(infff.name);
+
+
+//now let's try null
---
+++ practice.js
@@ -0,0 +1,13 @@
+const age = 30;
+if(age > 20){
+    console.log('Age is more than me');
+}
+else{
+    console.log('age is not so much');
+}
+//the false values are
+// undifined
+//null
+// ''
+//var = 0
+//NaN
